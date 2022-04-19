c6abfa7fc4e6237296870b274593fc12895c4767
Khent Mark Dahay <khentmark27dahay@gmail.com>
2022-04-19 16:07:43 +0800

    Adding arithmetic Understanding

---
+++ Python Fundamentals/Arithmetic.py
@@ -0,0 +1,19 @@
+# Python arithmetic operators 
+
+
+# Addition "+"
+# add, 1 + 2
+print(1+2) # using print statement to display the output 
+
+
+# Subtraction "-"
+# Subtract, 2 - 1
+print(2-1)
+
+
+# Division 
+# "/"
+# 2/1
+print(2/1) # Note *Division will produce float value 
+
+# 
